1f4b82e10c1b5b82f8e19c3b47038ff8888cf0d3
tttkouyou <bjkt1290248@gn.iwasaki.ac.jp>
2019-12-23 19:39:14 +0900

    We passed the data and shaped it.

--- lib/productButton.dart
+++ lib/productButton.dart
@@ -6,10 +6,14 @@ class ProductButton extends StatefulWidget {
     Key key,
     this.scaffoldKey,
     this.index,
+    this.purchasePlanList,
+    this.additem,
   }) : super(key: key);
 
   final scaffoldKey;
   final index;
+  final purchasePlanList;
+  final additem;
 
   @override
   ProductButtonState createState() => ProductButtonState();
@@ -17,29 +21,39 @@ class ProductButton extends StatefulWidget {
 
 class ProductButtonState extends State<ProductButton> {
   int count = 0;
-  List<String> ingredientName = [
-    '卵','肉','魚'];
+  List<String> ingredientName = ['卵', '肉', '魚'];
   List<Color> ingredientColor = [
     Colors.yellow[500],
     Colors.red[500],
     Colors.blue[500]
   ];
+
   @override
   Widget build(BuildContext context) {
-    return ButtonTheme(
-      minWidth: 83.0,
+    return Container(
+      padding: EdgeInsets.all(27),
+      width: 83.0,
       height: 83.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100.0),
+      ),
       child: RaisedButton(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25.0),
+          ),
           textColor: Colors.white,
-          padding: EdgeInsets.all(27),
+          // padding: EdgeInsets.all(27),
           color: ingredientColor[widget.index.toInt()],
-          child: Text(ingredientName[widget.index.toInt()],
-            style: TextStyle(fontSize: 50.0),
+          child: Text(
+            ingredientName[widget.index.toInt()],
+            style: TextStyle(fontSize: 30.0),
           ),
           onPressed: () {
+            widget.additem(ingredientName[widget.index]);
             count += 1;
             final snackBar = SnackBar(
-              content: Text(ingredientName[widget.index.toInt()] + 'を追加しました。合計：$count'),
+              content: Text(
+                  ingredientName[widget.index.toInt()] + 'を追加しました。合計：$count'),
               backgroundColor: Colors.orange[300],
               action: SnackBarAction(
                 label: '閉じる',
